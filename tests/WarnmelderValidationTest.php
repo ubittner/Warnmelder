43033e1370a458959e1d693b5e761ce3da31e9d7
Ulrich Bittner <ubittner@users.noreply.github.com>
2023-12-21 09:09:47 +0100

    added submodules

---
+++ tests/WarnmelderValidationTest.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace tests;
+
+use TestCaseSymconValidation;
+
+include_once __DIR__ . '/stubs/Validator.php';
+
+class WarnmelderValidationTest extends TestCaseSymconValidation
+{
+    public function testValidateLibrary(): void
+    {
+        $this->validateLibrary(__DIR__ . '/..');
+    }
+
+    public function testValidateModule_Warnmelder(): void
+    {
+        $this->validateModule(__DIR__ . '/../Warnmelder');
+    }
+}
